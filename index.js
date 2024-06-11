1c09665eb5781e1c26bd2eb5556bfb906ca1bb77
lucjovi2001 <171295225+lucjovi2001@users.noreply.github.com>
2024-06-11 13:32:32 -0700

    Add Player class
    Added a Player class with position and velocity
    attributes.

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ canvas.height = window.innerHeight
 
 context.fillStyle = 'black'
 context.fillRect(0, 0, canvas.width, canvas.height)
+
+class Player {
+    constructor({ position, velocity }) {
+        this.position = position // {x, y}
+        this.velocity = velocity
+    }
+
+    draw() {
+        context.fillStyle = 'red'
+        context.fillRect(this.position.x, this.position.y, 100, 100)
+    }
+}
+
+const player = new Player({
+    position: { x: canvas.width / 2, y: canvas.height / 2 },
+    velocity: { x: 0, y: 0 },
+})
+
+player.draw()
